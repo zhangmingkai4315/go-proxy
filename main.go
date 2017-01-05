70b919b9ffb246a7313cd16c6496e025632daf2b
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-01-05 15:58:16 +0800

    add v0.1 proxy to proxy all the web request to my website

--- main.go
+++ main.go
@@ -3,11 +3,29 @@ package main
 import (
 	"net"
 	"fmt"
+	"bufio"
+	"net/http"
+	"log"
 )
 
 func handleConnection(conn net.Conn){
-	fmt.Println("New Connection")
-	conn.Close();
+	defer conn.Close();
+	reader:=bufio.NewReader(conn)
+	if req,err:=http.ReadRequest(reader);err==nil{
+		  //connect to backend
+		  if be,err:=net.Dial("tcp","jsmean.com:80");err==nil{
+			  beReader:=bufio.NewReader(be)
+			  if err:=req.Write(be);err==nil{
+					//read the resonse from the backend
+				  if resp,err:=http.ReadResponse(beReader,req);err==nil{
+					  resp.Close=true
+					  if err:=resp.Write(conn);err==nil{
+						  log.Printf("%s:%d",req.URL.Path,resp.StatusCode)
+					  }
+					}
+				}
+		  }
+	}
 }
 func main() {
 	ln,err:=net.Listen("tcp",":8080")
@@ -18,7 +36,9 @@ func main() {
 		conn,err:=ln.Accept()
 		if err!=nil{
 			fmt.Println(err.Error())
+		}else{
+			go handleConnection(conn)
 		}
-		go handleConnection(conn)
+
 	}
 }
