7119d541af1b203b161fd392a067596ba992e79f
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2017-01-05 18:37:06 +0800

    v0.4 add pool for connection

--- main.go
+++ main.go
@@ -12,12 +12,47 @@ import (
 	"time"
 )
 
+
+type Backend struct {
+	net.Conn
+	Reader *bufio.Reader
+	Writer *bufio.Writer
+}
+
+var backendQueue chan *Backend
 var requestBytes map[string]int64
 var requestLock sync.Mutex
 
 func init(){
 	requestBytes=make(map[string]int64)
+	backendQueue =make(chan *Backend,10)
 }
+
+func getBackend()(*Backend,error){
+	select {
+	case be:=<-backendQueue:
+		return be,nil
+	case <-time.After(100*time.Millisecond):
+		be,err:=net.Dial("tcp","127.0.0.1:8081")
+		if err!=nil{
+			return nil,err
+		}
+		return &Backend{
+			Conn:be,
+			Reader:bufio.NewReader(be),
+			Writer:bufio.NewWriter(be),
+		},nil
+	}
+}
+
+func queueBackend(be *Backend){
+	select {
+	case backendQueue<-be:
+	case <-time.After(1*time.Second):
+		be.Close()
+	}
+}
+
 
 func updateStats(req *http.Request,resp *http.Response) int64{
 	requestLock.Lock()
@@ -45,11 +80,15 @@ func handleConnection(conn net.Conn){
 			}
 			return
 		}
-		if be,err:=net.Dial("tcp","localhost:8081");err==nil{
-			beReader:=bufio.NewReader(be)
-			if err:=req.Write(be);err==nil{
+
+		be,err:=getBackend()
+		if err!=nil{
+			return
+		}
+		if err:=req.Write(be);err==nil{
 				//read the resonse from the backend
-				if resp,err:=http.ReadResponse(beReader,req);err==nil{
+				be.Writer.Flush()
+				if resp,err:=http.ReadResponse(be.Reader,req);err==nil{
 					bytesLength:=updateStats(req,resp)
 					resp.Header.Set("X-Bytes-Length",strconv.FormatInt(bytesLength,10))
 					if err:=resp.Write(conn);err==nil{
@@ -60,8 +99,8 @@ func handleConnection(conn net.Conn){
 					}
 				}
 			}
+		go queueBackend(be)
 		}
-	}
 }
 func main() {
 	ln,err:=net.Listen("tcp",":8080")
@@ -70,12 +109,15 @@ func main() {
 		fmt.Println(err.Error())
 	}
 	// this ticker will print to map[string]int64 obejct every 10 seconds.
-	ticker := time.NewTicker(time.Second*10)
+	ticker := time.NewTicker(time.Second*20)
 	go func() {
 		for t := range ticker.C {
-			fmt.Printf("[%s] Stats are %+v\n",t.String(),requestBytes)
+			fmt.Printf("\n[%s] Stats are %+v\n",t.String(),requestBytes)
 		}
 	}()
+
+
+
 
 	for {
 		if conn,err:=ln.Accept();err==nil{
